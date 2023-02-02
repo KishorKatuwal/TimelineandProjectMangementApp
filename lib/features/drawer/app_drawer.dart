66d0fa78b3e630f6a6ff4e0357a6f1433398b660
Kishor <kishorkatuwal96@gmail.com>
2023-02-02 20:50:19 +0545

    drawer added on home page

---
+++ lib/features/drawer/app_drawer.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:timelineandprojectmanagementapp/constants/global_variables.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      // color: const Color.fromRGBO(242, 244, 255, 1),
+      child: Drawer(
+        backgroundColor: const Color.fromRGBO(242, 244, 255, 1),
+        child: ListView(
+          children: [
+            Container(
+              color: GlobalVariables.mainColor,
+              child: const ListTile(
+                title: Text(
+                  "Timeline and Project Management App",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+            const Divider(
+              height: 3,
+              thickness: 3,
+            ),
+            const ListTile(
+              leading: Icon(Icons.newspaper),
+              title: Text(
+                "View Exam",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.school_outlined),
+              title: Text(
+                "View Results",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.calendar_month_outlined),
+              title: Text(
+                "View Events",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.assignment_outlined),
+              title: Text(
+                "View Projects",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 150,
+            ),
+            const Divider(
+              height: 3,
+              thickness: 3,
+              color: Colors.black45,
+            ),
+            const ListTile(
+              leading: Icon(Icons.feedback_outlined),
+              title: Text(
+                "Provide Feedback",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.settings),
+              title: Text(
+                "Settings",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+           const ListTile(
+              leading: Icon(Icons.info_outline),
+              title: Text(
+                "About App",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.logout),
+              title: Text(
+                "Log Out",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
